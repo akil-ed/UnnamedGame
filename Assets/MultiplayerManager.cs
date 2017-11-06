7ea75d7b27ae60f2e2312e9ab98be86c3bd66466
DESKTOP-QK0H43P\kanna <kannanjan21@gmail.com>
2017-11-06 19:40:24 +0530

    multiplayer begining.

---
+++ Assets/MultiplayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerManager : MonoBehaviour {
+
+	public Transform spawnpoint;
+
+	// Use this for initialization
+	void Start () {
+		if(PhotonNetwork.isMasterClient)
+			PhotonNetwork.Instantiate ("player 1",spawnpoint.position,spawnpoint.rotation,new byte());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Xonix/Scripts/Multiplayer/PhotonHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhotonHelper : MonoBehaviour 
+{
+	public static PhotonHelper instance;
+
+	void Start()
+	{
+		instance = this;
+		PhotonNetwork.ConnectUsingSettings (Application.version);
+	}
+
+
+	void OnConnectedToMaster()
+	{
+		print ("Connected to server");
+		CreateOrJoinAGame ();
+	}
+
+	public void CreateOrJoinAGame()
+	{
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		print ("Join random room failed!");
+		PhotonNetwork.CreateRoom (null,new RoomOptions(){MaxPlayers = 2},null);
+	}
+
+	void OnCreatedRoom()
+	{
+		print ("room created!");
+	}
+
+	void OnJoinedRoom()
+	{
+		print ("room joined!");
+		StartCoroutine (WaitForPlayers());
+		//PhotonNetwork.Instantiate ("player",Vector3.zero,Quaternion.identity,new byte());
+	}
+
+	void OnConnectionFail(DisconnectCause cause)
+	{
+		print (cause.ToString());
+	}
+
+	IEnumerator WaitForPlayers()
+	{
+		while (PhotonNetwork.playerList.Length != PhotonNetwork.room.MaxPlayers)
+			yield return null;
+		SceneManager.LoadScene ("Main_2");
+	}
+
+}
